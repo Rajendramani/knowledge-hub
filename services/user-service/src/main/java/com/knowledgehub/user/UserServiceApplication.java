075e44e1ac794ff4398a369da12afb0f6b3b6e6f
Rajendramani <2009671+Rajendramani@users.noreply.github.com>
2026-04-08 21:31:39 +0530

    Add Parent POM

---
+++ services/user-service/src/main/java/com/knowledgehub/user/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.knowledgehub.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
